333ad2c254dc13b429651e387704641a92646b3d
Eng. Juan Camilo Gómez Cadavid MSc <kmilo17pet@gmail.com>
2019-04-26 15:16:18 -0500

    increased release to 4.6.8

--- src/core/QuarkTS.c
+++ src/core/QuarkTS.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
  *  QuarkTS - A Non-Preemptive Task Scheduler for low-range MCUs
- *  Version : 4.6.7g
+ *  Version : 4.6.8
  *  Copyright (C) 2012 Eng. Juan Camilo Gomez C. MSc. (kmilo17pet@gmail.com)
  *
  *  QuarkTS is free software: you can redistribute it and/or modify it
