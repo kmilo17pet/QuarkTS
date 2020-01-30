d41610074382b8a75725b44cf80492e5fd6eae98
Eng. Juan Camilo Gómez Cadavid MSc <kmilo17pet@gmail.com>
2020-01-30 11:52:08 -0500

    updated user manual

--- build_options.mk
+++ build_options.mk
@@ -14,7 +14,7 @@ LD = gcc
 # Flags to pass to the compiler for release builds
 CFLAGS ?= -Wall  -fstrict-aliasing -O2 -std=c89 -pedantic -D_POSIX_C_SOURCE=199309L -MD -Wstrict-aliasing
 # Flags to pass to the linker
-LFLAGS ?= -lm
+LFLAGS ?= -lm 
 # Output directories
 OBJ_DIR := obj
 BIN_DIR := bin
