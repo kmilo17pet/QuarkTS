d9d9d240a9f8d68f7b348ec6015c231954d7dc6e
kmilo17pet <kmilo17pet@gmail.com>
2020-07-24 11:39:56 -0500

    trace header extended func comp.

--- Makefile
+++ Makefile
@@ -8,37 +8,6 @@ include build_options.mk
 ###################################################################################################
 ### Do NOT touch the lines below , use the build_options.mk file to change the compile behavior ###
 ###################################################################################################
-COM_COLOR   = \033[0;34m
-OBJ_COLOR   = \033[0;36m
-OK_COLOR    = \033[0;32m
-ERROR_COLOR = \033[0;31m
-WARN_COLOR  = \033[0;33m
-NO_COLOR    = \033[m
-
-OK_STRING    = "[OK]"
-ERROR_STRING = "[ERROR]"
-WARN_STRING  = "[WARNING]"
-COM_STRING   = "Compiling"
-SUCCESS_STRING = "Success!"
-LINK_STRING = "Linking..."
-
-define run_and_test
-echo $(1); \
-printf "%b" "$(COM_COLOR)$(COM_STRING) $(OBJ_COLOR)$(@F)$(NO_COLOR)\r"; \
-$(1) 2> $@.log; \
-RESULT=$$?; \
-if [ $$RESULT -ne 0 ]; then \
-  printf "%-60b%b" "$(COM_COLOR)$(COM_STRING)$(OBJ_COLOR) $@" "$(ERROR_COLOR)$(ERROR_STRING)$(NO_COLOR)\n"   ; \
-elif [ -s $@.log ]; then \
-  printf "%-60b%b" "$(COM_COLOR)$(COM_STRING)$(OBJ_COLOR) $@" "$(WARN_COLOR)$(WARN_STRING)$(NO_COLOR)\n"   ; \
-else  \
-  printf "%-60b%b" "$(COM_COLOR)$(COM_STRING)$(OBJ_COLOR) $(@F)" "$(OK_COLOR)$(OK_STRING)$(NO_COLOR)\n"   ; \
-fi; \
-cat $@.log; \
-rm -f $@.log; \
-exit $$RESULT
-endef
-
 INC 	:= 	$(sort -I. $(addprefix -I./,$(dir  $(wildcard *.h */*.h */*/*.h */*/*/*.h)   )) )
 SRC 	:= 	$(wildcard src/**/*.c)
 OBJ 	:= 	$(addprefix $(OBJ_DIR)/,$(SRC:.c=$(OBJ_EXT)))
@@ -49,14 +18,11 @@ OUT 	= 	$(BIN_DIR)/$(notdir $(CURDIR))
 
 $(OUT): $(OBJ)
 	@mkdir -p $(dir $@)
-	@printf "%b" "$(COM_COLOR)$(LINK_STRING)$(NO_COLOR)\n" ; 
 	$(LD) $^ $(LFLAGS) -o $@
-	@echo "-------------------------------"
-	@printf "%b" "$(COM_COLOR)$(SUCCESS_STRING)$(NO_COLOR)\n"  ; 
 
 $(OBJ_DIR)/%$(OBJ_EXT): %.c
 	@mkdir -p $(dir $@)
-	@$(call run_and_test, $(CC) $(CFLAGS) $(INC)  -c $< -o $@ )
+	$(CC) $(CFLAGS) $(INC)  -c $< -o $@ 
 	
 rebuild:
 	$(MAKE) clean
--- src/os/include/qtrace.h
+++ src/os/include/qtrace.h
@@ -12,28 +12,36 @@
     #define _qSTRINGIFY(x) #x
     #define _qTOSTRING(x) _qSTRINGIFY(x)
 
+    #ifndef _QTRACE_FUNC
+        #if defined __cplusplus && defined __GNUC__ /* Use g++'s demangled names in C++.  */
+            #if  __GNUC__ >= 2
+                #define _QTRACE_FUNC __PRETTY_FUNCTION__
+            #else
+                #define _QTRACE_FUNC   __func__
+            #endif       
+        #elif defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+            # define _QTRACE_FUNC __PRETTY_FUNCTION__
+        #elif defined(__DMC__) && (__DMC__ >= 0x810)
+            # define _QTRACE_FUNC __PRETTY_FUNCTION__
+        #elif defined(__FUNCSIG__)
+            # define _QTRACE_FUNC __FUNCSIG__
+        #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+            # define _QTRACE_FUNC __FUNCTION__
+        #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+            # define _QTRACE_FUNC __FUNC__
+        #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+            # define _QTRACE_FUNC __func__
+        #elif defined(__cplusplus) && (__cplusplus >= 201103)
+            # define _QTRACE_FUNC __func__                
+        #else /* failed to detect __func__ support.  */
+            #define _QTRACE_FUNC ((char *) 0)
+        #endif
+    # endif
+
     #if ( Q_DEBUGTRACE_FULL == 1 )
-        #ifndef _QTRACE_FUNC
-            #if defined __cplusplus && defined __GNUC__ /* Use g++'s demangled names in C++.  */
-                #if  __GNUC__ >= 2
-                    #define _QTRACE_FUNC __PRETTY_FUNCTION__
-                #else
-                    #define _QTRACE_FUNC   __func__
-                #endif       
-            #elif defined __STDC_VERSION__ 
-                #if __STDC_VERSION__ >= 199901L /* C99 requires the use of __func__.  */
-                    #define _QTRACE_FUNC __func__
-                #else
-                    #define _QTRACE_FUNC ((char *) 0)
-                #endif
-            #else /* failed to detect __func__ support.  */
-                #define _QTRACE_FUNC ((char *) 0)
-            #endif
-        # endif
         #define _qAT() "[" __FILE__ ":" _qTOSTRING(__LINE__) "] " 
     #else
         #define _qAT()         ""
-        #define _QTRACE_FUNC    ((char *) 0)
     #endif
 
     #ifndef Message
