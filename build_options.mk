74f4cc50b768e05c7b93ff8039ee044410c0844b
Eng. Juan Camilo Gómez Cadavid MSc <kmilo17pet@gmail.com>
2021-04-29 11:32:28 -0500

    submodule update. added extra MISRAC2012 checks.

--- build_options.mk
+++ build_options.mk
@@ -12,10 +12,10 @@ CC = gcc
 # The linker
 LD = gcc
 # Flags to pass to the compiler for release builds
-EXTRAFLAGS ?= -flto -Wextra -Wimplicit-fallthrough=0 -Wformat-security -Wduplicated-cond -Wfloat-equal -Wshadow -Wconversion -Wsign-conversion -Wjump-misses-init -Wlogical-not-parentheses -Wnull-dereference  -Wnull-dereference -Wstringop-overflow -fprofile-arcs -ftest-coverage 
-CFLAGS ?= -Wall $(EXTRAFLAGS)  -fstrict-aliasing -O2 -std=c99 -D_POSIX_C_SOURCE=199309L -MD -Wstrict-aliasing -DQLIST_D_HANDLING
+EXTRAFLAGS ?= -flto -Wextra -Wimplicit-fallthrough=0 -Wformat-security -Wduplicated-cond -Wfloat-equal -Wshadow -Wconversion -Wsign-conversion -Wjump-misses-init -Wlogical-not-parentheses -Wnull-dereference  -Wnull-dereference -Wstringop-overflow 
+CFLAGS ?= -fdump-rtl-expand -Wall $(EXTRAFLAGS)  -fstrict-aliasing -O2 -std=c99 -D_POSIX_C_SOURCE=199309L -MD -Wstrict-aliasing -DQLIST_D_HANDLING
 # Flags to pass to the linker
-LFLAGS ?= -lm  -fprofile-generate
+LFLAGS ?= -lm  
 # Output directories
 OBJ_DIR := obj
 BIN_DIR := bin
