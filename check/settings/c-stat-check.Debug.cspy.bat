eccf1105cfb6f1fd022de5d7f6f6fa35bc168437
kmilo17pet <kmilo17pet@gmail.com>
2021-08-17 18:38:54 -0500

    cstat prj updated. Submodules update.

--- check/settings/c-stat-check.Debug.cspy.bat
+++ check/settings/c-stat-check.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
+"C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
 
 @echo off 
 :end
--- check/settings/c-stat-check.Debug.cspy.ps1
+++ check/settings/c-stat-check.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\Camilo\Documents\GitHub\QuarkTS\check\settings\c-stat-check.Debug.driver.xcl" 
 }
